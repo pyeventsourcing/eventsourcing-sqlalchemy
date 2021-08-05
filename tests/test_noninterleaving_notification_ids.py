ed6f9d19818fd780fc8a0be81a1e8123dbff8224
John Bywater <john.bywater@appropriatesoftware.net>
2021-08-05 16:17:50 +0100

    Added test for non-interleaving of notification IDs in atomic inserts.

---
+++ tests/test_noninterleaving_notification_ids.py
@@ -0,0 +1,65 @@
+from eventsourcing.postgres import PostgresDatastore
+from eventsourcing.tests.noninterleaving_notification_ids_testcase import (
+    NonInterleavingNotificationIDsBaseCase,
+)
+from eventsourcing.tests.ramdisk import tmpfile_uris
+from eventsourcing.tests.test_postgres import drop_postgres_table
+
+from eventsourcing_sqlalchemy.datastore import SqlAlchemyDatastore
+from eventsourcing_sqlalchemy.recorders import SqlAlchemyApplicationRecorder
+
+
+class TestNonInterleaving(NonInterleavingNotificationIDsBaseCase):
+    sqlalchemy_db_url = "sqlite:///:memory:"
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.datastore = SqlAlchemyDatastore(self.sqlalchemy_db_url)
+
+    def create_recorder(self):
+        recorder = SqlAlchemyApplicationRecorder(
+            datastore=self.datastore,
+            events_table_name="stored_events"
+        )
+        recorder.create_table()
+        return recorder
+
+
+class TestNonInterleavingSQLiteFileDB(TestNonInterleaving):
+    insert_num = 1000
+
+    def setUp(self) -> None:
+        uris = tmpfile_uris()
+        db_uri = next(uris)
+        db_uri = db_uri.lstrip("file:")
+        self.sqlalchemy_db_url = f"sqlite:///{db_uri}"
+        super().setUp()
+
+
+class TestNonInterleavingPostgres(TestNonInterleaving):
+    insert_num = 5000
+    sqlalchemy_db_url = (
+        "postgresql://eventsourcing:eventsourcing@localhost:5432/eventsourcing_sqlalchemy"
+    )
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.drop_tables()
+
+    def tearDown(self) -> None:
+        self.drop_tables()
+        super().tearDown()
+
+    def drop_tables(self):
+        datastore = PostgresDatastore(
+            dbname="eventsourcing_sqlalchemy",
+            host="127.0.0.1",
+            port="5432",
+            user="eventsourcing",
+            password="eventsourcing",
+        )
+        drop_postgres_table(datastore, "stored_events")
+        drop_postgres_table(datastore, "stored_events")
+
+
+del NonInterleavingNotificationIDsBaseCase
