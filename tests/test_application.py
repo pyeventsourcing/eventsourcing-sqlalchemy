c4e25a53c723e87526ebada06e005601e53e6798
johnbywater <john.bywater@appropriatesoftware.net>
2023-10-23 08:10:48 +0100

    Added tests for using scoped sessions with flask-sqlalchemy and fastapi-sqlalchemy.

--- tests/test_application.py
+++ tests/test_application.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 import os
+from typing import Any
 from unittest import TestCase
 
+import fastapi_sqlalchemy
 from eventsourcing.application import AggregateNotFound, Application
 from eventsourcing.domain import Aggregate
 from eventsourcing.postgres import PostgresDatastore
 from eventsourcing.tests.application import TIMEIT_FACTOR, ExampleApplicationTestCase
 from eventsourcing.tests.postgres_utils import drop_postgres_table
 from eventsourcing.utils import get_topic
-
-# from fastapi_sqlalchemy import db
-# from flask_sqlalchemy import SQLAlchemy
+from fastapi_sqlalchemy import DBSessionMiddleware
 from sqlalchemy.orm import scoped_session
+
+try:
+    from sqlalchemy.orm import declarative_base  # type: ignore
+except ImportError:
+    from sqlalchemy.ext.declarative import declarative_base
 
 from eventsourcing_sqlalchemy.factory import Factory
 from eventsourcing_sqlalchemy.recorders import SQLAlchemyApplicationRecorder
@@ -141,44 +146,126 @@ class TestApplicationWithSQLAlchemy(ExampleApplicationTestCase):
         # After request.
         session.remove()
 
-    # def test_fastapi_sqlalchemy(self) -> None:
-    #     db
-    #
-    # def test_flask_sqlalchemy(self) -> None:
-    #     SQLAlchemy
+    def test_flask_sqlalchemy(self) -> None:  # 40%
+        # flask_sqlalchemy.SQLAlchemy.session  #  <- this is an SQLA scoped_session
 
-    #     # Get aggregate.
-    #     self.assertIsInstance(app.repository.get(aggregate.id), Aggregate)
+        del os.environ["SQLALCHEMY_URL"]
+
+        from flask import Flask
+        from flask_sqlalchemy import SQLAlchemy
+
+        flask_app = Flask(__name__)
+        flask_app.config["SQLALCHEMY_DATABASE_URI"] = self.sqlalchemy_database_url
+
+        Base = declarative_base()
+        db = SQLAlchemy(flask_app, model_class=Base)
+
+        es_app = Application()
+        assert isinstance(
+            es_app.recorder, SQLAlchemyApplicationRecorder
+        )  # For IDE/mypy.
+        assert isinstance(es_app.factory, Factory)  # For IDE/mypy.
+
+        # Set up database.
+        with flask_app.app_context():
+            es_app.factory.datastore.set_scoped_session(db.session)
+            es_app.recorder.create_table()
+
+            # Handle request.
+            aggregate = Aggregate()
+            es_app.save(aggregate)
+            es_app.repository.get(aggregate.id)
+            db.session.commit()
+
+            # After request.
+            db.session.remove()
+
+            # Handle request.
+            es_app.repository.get(aggregate.id)
+
+            # After request.
+            db.session.remove()
+
     #
-    # # Create an aggregate - autoflush=False with session.no_autoflush.
-    # with app.recorder.datastore.transaction(commit=True) as session:
-    #     with session.no_autoflush:
-    #         self.assertFalse(session.autoflush)
-    #         aggregate = Aggregate()
-    #         app.save(aggregate)
+    def test_fastapi_sqlalchemy(self) -> None:  # 20%
+        # fastapi_sqlalchemy.db.session  # <- this is not a scoped_session,
+        # it's a property that returns a new session when accessed
+
+        del os.environ["SQLALCHEMY_URL"]
+
+        from fastapi import FastAPI
+        from fastapi_sqlalchemy import db
+
+        fastapi_app = FastAPI()
+
+        fastapi_app.add_middleware(DBSessionMiddleware, db_url="sqlite://")
+
+        class FakeScopedSession(scoped_session):
+            def __init__(self, db: Any) -> None:
+                self._db = db
+
+            def __getattribute__(self, item: str) -> None:
+                if item == "_db":
+                    return super().__getattribute__(item)
+                else:
+                    return getattr(self._db.session, item)
+
+        Session = FakeScopedSession(fastapi_sqlalchemy.db)
+
+        es_app = Application()
+        assert isinstance(
+            es_app.recorder, SQLAlchemyApplicationRecorder
+        )  # For IDE/mypy.
+        assert isinstance(es_app.factory, Factory)  # For IDE/mypy.
+
+        fastapi_app.build_middleware_stack()
+
+        @fastapi_app.get(path="/index")
+        def index() -> str:
+            # aggregate = Aggregate()
+            # es_app.save(aggregate)
+            # es_app.repository.get(aggregate.id)
+            return ""
+
+        # Set up database.
+        with db(commit_on_exit=True):
+            es_app.factory.datastore.set_scoped_session(Session)
+            es_app.recorder.create_table()
+
+        with db(commit_on_exit=True):
+            # Handle request.
+            aggregate = Aggregate()
+            es_app.save(aggregate)
+            es_app.repository.get(aggregate.id)
+
+        with db():
+            es_app.repository.get(aggregate.id)
+
+        with db(commit_on_exit=False):
+            # Handle request.
+            aggregate = Aggregate()
+            es_app.save(aggregate)
+            es_app.repository.get(aggregate.id)
+
+        with db():
+            with self.assertRaises(AggregateNotFound):
+                es_app.repository.get(aggregate.id)
+
     #
-    #     # Get aggregate.
-    #     self.assertIsInstance(app.repository.get(aggregate.id), Aggregate)
+    # def test_tornado_sqlalchemy(self) -> None:  # 3%
+    #     tornado_sqlalchemy.SessionMixin.session  #  <- this just calls an SQLA sessionmaker
     #
-    # # Create an aggregate - autoflush=False with session.no_autoflush.
-    # with app.recorder.datastore.transaction(commit=True) as session:
-    #     with session.no_autoflush:
-    #         self.assertFalse(session.autoflush)
-    #         aggregate = Aggregate()
-    #         app.save(aggregate)
+    # def test_web2py_sqlalchemy(self) -> None:  # 3%
     #
-    #     # Get aggregate.
-    #     self.assertIsInstance(app.repository.get(aggregate.id), Aggregate)
+    # def test_bottle_sqlalchemy(self) -> None:  # 2%
     #
-    # # Create an aggregate - autoflush=False after configuring session maker.
-    # app.recorder.datastore.session_maker.kw["autoflush"] = False
-    # with app.recorder.datastore.transaction(commit=True) as session:
-    #     self.assertFalse(session.autoflush)
-    #     aggregate = Aggregate()
-    #     app.save(aggregate)
+    # def test_cherrypy_sqlalchemy(self) -> None:  # 2%
+    #     cherrypy.request.db  #  <- this is an SQLA scoped_session
     #
-    #     # Get aggregate.
-    #     self.assertIsInstance(app.repository.get(aggregate.id), Aggregate)
+    # def test_falcon_sqlalchemy(self) -> None:  # 2%
+    #
+    # def test_pyramid_sqlalchemy(self) -> None:  # 1%
+    #     pyramid_sqlalchemy.Session  #  <- this is an SQLA scoped_session
 
 
 class TestWithPostgres(TestApplicationWithSQLAlchemy):
